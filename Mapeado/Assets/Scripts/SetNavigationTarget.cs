2dd7554e2e58a073a35090cccd0b4c75b04d5f3f
ailenaguino <ailenaguino@gmail.com>
2022-09-03 19:10:10 -0300

    Parte 4

---
+++ Mapeado/Assets/Scripts/SetNavigationTarget.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetNavigationTarget : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera topDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+    [SerializeField]
+    private GameObject indicator;
+
+    private NavMeshPath path;
+    private LineRenderer line;
+
+    private bool lineToggle = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) {
+            lineToggle = !lineToggle;
+        }*/
+
+        if (lineToggle)
+        {
+            NavMesh.CalculatePath(indicator.transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+            line.enabled = true;
+        }
+    }
+}
